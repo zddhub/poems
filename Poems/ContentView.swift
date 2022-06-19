2ccdd0d2df6228eb8de91c58e71d809ccdce201f
zdd <zddhub@gmail.com>
2022-06-19 12:42:38 +0800

    Initial Commit

---
+++ Poems/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Poems
+//
+//  Created by Dongdong Zhang on 2022/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poems/PoemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemsApp.swift
+//  Poems
+//
+//  Created by Dongdong Zhang on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct PoemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
